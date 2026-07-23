59d6ac9b481ab9452da32e4ccdb8d060801c97e8
Thor Hanssen <69870370+Chaooz@users.noreply.github.com>
2026-07-23 17:08:02 +0200

    DW-59 remove all Tailwind references (#19)

--- DFWeb.FR.Tailwind/run_tailwind.sh
+++
@@ -1,5 +0,0 @@
-!/bin/bash
-#
-# Script to start the post process of CSS for tailwind
-#
-npx tailwindcss -c ./wwwroot/lib/tailwind/tailwind.config.js -i ./wwwroot/lib/tailwind/tailwind_input.css -o ./wwwroot/css/tailwind_output.css --watch
